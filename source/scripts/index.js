409ef3a4b8cd96688708e43f42d9869f28136ab2
VladissGernet <skeimorr@gmail.com>
2024-01-31 16:44:26 +0400

    nouislider styling

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -3,3 +3,19 @@ import { initSlider } from './slider.js';
 
 initMenu();
 initSlider();
+
+const priceSlider = document.querySelector('.filter-range__controls');
+priceSlider.innerHTML = '';
+noUiSlider.create(priceSlider, {
+  start: [0, 920],
+  step: 5,
+  connect: true,
+  range: {
+    'min': 0,
+    'max': 1000
+  }
+});
+
+priceSlider.noUiSlider.on('update', () => {
+  console.log(priceSlider.noUiSlider.get());
+});
