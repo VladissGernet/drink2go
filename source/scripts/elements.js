aa35438ddf84c24eeb50d720d9a8bb9eaca7dd47
VladissGernet <skeimorr@gmail.com>
2024-01-31 13:54:58 +0400

    slider added

--- source/scripts/elements.js
+++ source/scripts/elements.js
@@ -1,5 +1,22 @@
 const siteNavigation = document.querySelector('.site-navigation');
 const menuButton = document.querySelector('.js-toggle-button');
 const menuButtonIcon = menuButton.querySelector('use');
+const heroBlock = document.querySelector('.hero');
+const sliderList = heroBlock.querySelector('.slider');
+const sliderItems = sliderList.querySelectorAll('.slider__item');
+const sliderPagination = heroBlock.querySelector('.slider-pagination');
+const sliderPaginationItems = sliderPagination.querySelectorAll('.slider-pagination__button');
+const sliderPrevButton = heroBlock.querySelector('.slider-button-prev');
+const sliderNextButton = heroBlock.querySelector('.slider-button-next');
 
-export { siteNavigation, menuButton, menuButtonIcon };
+export {
+  siteNavigation,
+  menuButton,
+  menuButtonIcon,
+  sliderList,
+  sliderItems,
+  sliderPagination,
+  sliderPaginationItems,
+  sliderPrevButton,
+  sliderNextButton
+};
---
+++ source/scripts/slider.js
@@ -0,0 +1,91 @@
+import {
+  sliderList,
+  sliderItems,
+  sliderPagination,
+  sliderPaginationItems,
+  sliderPrevButton,
+  sliderNextButton
+} from './elements.js';
+
+const initSlider = () => {
+  const checkShownSlide = (index, button) => () => {
+    const slide = sliderList.querySelectorAll('.slider__item')[index];
+    if (slide.classList.contains('slider__item--hidden') === false) {
+      button.disabled = true;
+      return;
+    }
+    button.disabled = false;
+  };
+  const firstSlideNumber = 0;
+  const lastSlideNumber = sliderItems.length - 1;
+
+  const isFirstSlideShown = checkShownSlide(firstSlideNumber, sliderPrevButton);
+  const isLastSlideShown = checkShownSlide(lastSlideNumber, sliderNextButton);
+
+  isFirstSlideShown();
+  isLastSlideShown();
+
+  const onNextButtonClick = () => {
+    const updatedSliderList = sliderList.querySelectorAll('.slider__item');
+    let currentSliderIndex;
+    updatedSliderList.forEach((slide, index) => {
+      if (slide.classList.contains('slider__item--hidden') === false) {
+        currentSliderIndex = index;
+      }
+    });
+    const isNotLastSlideIndex = currentSliderIndex + 1 > updatedSliderList.length - 1 === false;
+    if (isNotLastSlideIndex) {
+      const newSliderIndex = currentSliderIndex + 1;
+      updatedSliderList[currentSliderIndex].classList.add('slider__item--hidden');
+      updatedSliderList[newSliderIndex].classList.remove('slider__item--hidden');
+      sliderPaginationItems.forEach((item) => item.classList.remove('slider-pagination__button--active'));
+      sliderPaginationItems[newSliderIndex].classList.add('slider-pagination__button--active');
+      isFirstSlideShown();
+    }
+    isLastSlideShown();
+  };
+  sliderNextButton.addEventListener('click', onNextButtonClick);
+
+  const onPrevButtonClick = () => {
+    const updatedSliderList = sliderList.querySelectorAll('.slider__item');
+    let currentSliderIndex;
+    updatedSliderList.forEach((slide, index) => {
+      if (slide.classList.contains('slider__item--hidden') === false) {
+        currentSliderIndex = index;
+      }
+    });
+    const isNotFirstSlideIndex = currentSliderIndex > 0;
+    if (isNotFirstSlideIndex) {
+      const newSliderIndex = currentSliderIndex - 1;
+      updatedSliderList[currentSliderIndex].classList.add('slider__item--hidden');
+      updatedSliderList[newSliderIndex].classList.remove('slider__item--hidden');
+      sliderPaginationItems.forEach((item) => item.classList.remove('slider-pagination__button--active'));
+      sliderPaginationItems[newSliderIndex].classList.add('slider-pagination__button--active');
+      isLastSlideShown();
+    }
+    isFirstSlideShown();
+  };
+  sliderPrevButton.addEventListener('click', onPrevButtonClick);
+
+  const onPaginationClick = (evt) => {
+    const selectedPaginationButton = evt.target;
+    if (selectedPaginationButton.classList.contains('slider-pagination__button') === false) {
+      return;
+    }
+    const isNotActiveButton = selectedPaginationButton.classList.contains('slider-pagination__button--active') === false;
+    if (isNotActiveButton) {
+      const activeButton = sliderPagination.querySelector('.slider-pagination__button--active');
+      activeButton.classList.remove('slider-pagination__button--active');
+      selectedPaginationButton.classList.add('slider-pagination__button--active');
+      const newSliderPaginationItems = sliderPagination.querySelectorAll('.slider-pagination__button');
+      const activeButtonIndex = Array.from(newSliderPaginationItems).findIndex((item) => item.classList.contains('slider-pagination__button--active'));
+      sliderItems.forEach((item) => item.classList.add('slider__item--hidden'));
+      sliderItems[activeButtonIndex].classList.remove('slider__item--hidden');
+      isFirstSlideShown();
+      isLastSlideShown();
+    }
+  };
+  sliderPagination.addEventListener('click', onPaginationClick);
+};
+
+export { initSlider };
